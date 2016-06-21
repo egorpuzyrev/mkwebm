81ce660e7a6cb7438b85aecfe8cfcadc58a12c3e
egorpuzyrev <egorpuzyrev@gmail.com>
2016-06-22 04:24:25 +0700

    Fix

--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
 from setuptools import setup
 
-<<<<<<< HEAD
 setup(name='mkwebm',
       version='0.1',
       description='Creates webm from image and music',
@@ -8,13 +7,4 @@ setup(name='mkwebm',
       author_email='egorpuzyrev@gmail.com',
 #      url='http://www.python.org/sigs/distutils-sig/',
       install_requires=['bottle>=0.12.9'],
-=======
-setup(name='YourAppName',
-      version='1.0',
-      description='OpenShift App',
-      author='Your Name',
-      author_email='example@example.com',
-      url='http://www.python.org/sigs/distutils-sig/',
-#      install_requires=['Django>=1.3'],
->>>>>>> b86bad43f3854665e297c28a35dc726c3bdbbf4f
      )
