d13f0a2ff50357b73d3fc4a21528c0b623249a89
egorpuzyrev <egorpuzyrev@gmail.com>
2016-06-23 22:51:40 +0700

    Test cron

--- .openshift/cron/minutely/wipe_webms.sh
+++ .openshift/cron/minutely/wipe_webms.sh
@@ -5,5 +5,5 @@ fi
 if [[ $(find $OPENSHIFT_DATA_DIR/last_run -mmin +29) ]]; then #run every 30 mins
   rm -f $OPENSHIFT_DATA_DIR/last_run
   touch $OPENSHIFT_DATA_DIR/last_run
-  find $OPENSHIFT_DATA_DIR/mkwebm/webms/ -type f -mtime +30 -exec rm {} \;
+  find $OPENSHIFT_DATA_DIR/mkwebm/webms/ -type f -mtime +30 -exec echo {} \;
 fi
