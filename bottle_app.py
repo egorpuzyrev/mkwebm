20e41b0f2d7e998a097b8c366f7b4db222d89367
egorpuzyrev <egorpuzyrev@gmail.com>
2016-06-25 21:08:18 +0700

    Update

--- bottle_app.py
+++ bottle_app.py
@@ -7,7 +7,7 @@ import shlex
 import subprocess
 
 import bottle
-from bottle import default_app, route, get, post, static_file, request, view, url, template, redirect
+from bottle import default_app, route, get, post, static_file, request, view, url, template, redirect, response
 
 # ~VIRTENV = os.environ.get('OPENSHIFT_PYTHON_DIR', '.') + '/virtenv/'
 # ~virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
@@ -136,11 +136,14 @@ def get_params():
     # ~print('template:\n', template('<link href="{{ url("webms", filename=basename) }}" type="video/webm"/>', url=url, basename=basename))
     # ~return template('<html><body><link href="{{ url("webms", filename=basename) }}" type="video/webm"/></body></html>', url=url, basename=basename)
     print('redirecting to /webms/{}'.format(basename))
+    response.set_header('Location', '/webms/{}'.format(basename))
     # ~print('template:', '<html><body><video controls><source src="/webms/{}"/></video></body></html>'.format(basename))
-    # ~redirect('/webms/{}'.format(basename), code=303)
-    give_webm(basename)
+    redirect('/webms/{}'.format(basename), code=303)
+    # ~give_webm(basename)
     # ~return template('<html><body><video controls><source src="/webms/{}"/></video></body></html>'.format(basename))
     # ~return '<html><body><video controls><source src="/webms/{}"/></video></body></html>'.format(basename)
+
+    # ~return {'url': '/webms/{}'.format(basename)}
 
 @route('/dashboard')
 @view('secure_page')
