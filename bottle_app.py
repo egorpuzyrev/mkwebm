f597614f99fe6f96cd70ee33d5121691f97da101
egorpuzyrev <egorpuzyrev@gmail.com>
2016-10-06 00:41:23 +0300

    Fixies for gevent

--- bottle_app.py
+++ bottle_app.py
@@ -187,9 +187,28 @@ def show_webms_list():
     webms_list = [os.path.basename(i) for i in glob.glob(os.path.join(WEBM_CACHE_DIR, '*.webm'))]
     return {'webms_list': webms_list}
 
+
+
+virtenv = os.environ['OPENSHIFT_PYTHON_DIR'] + '/virtenv/'
+virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
+try:
+  # See: http://stackoverflow.com/questions/23418735/using-python-3-3-in-openshifts-book-example?noredirect=1#comment35908657_23418735
+  #execfile(virtualenv, dict(__file__=virtualenv)) # for Python v2.7
+  #exec(compile(open(virtualenv, 'rb').read(), virtualenv, 'exec'), dict(__file__=virtualenv)) # for Python v3.3
+  # Multi-Line for Python v3.3:
+  exec_namespace = dict(__file__=virtualenv)
+  with open(virtualenv, 'rb') as exec_file:
+    file_contents = exec_file.read()
+  compiled_code = compile(file_contents, virtualenv, 'exec')
+  exec(compiled_code, exec_namespace)
+except IOError:
+  pass
+
+
 ip = os.environ.get('OPENSHIFT_PYTHON_IP', 'localhost')
 port = int(os.environ.get('OPENSHIFT_PYTHON_PORT', 8051))
 
 # ~run(reloader=True)
 application = default_app()
-application.run(host=ip, port=port, server='gevent', reloader=True)
+# ~application.run(host=ip, port=port, server='gevent', reloader=True)
+application.run(host=ip, port=port, server='gevent')
