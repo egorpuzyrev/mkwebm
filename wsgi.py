125577a0b9bce9642c335a09755a00843f9314f1
egorpuzyrev <egorpuzyrev@gmail.com>
2016-10-06 01:42:26 +0300

    Fixed errors

--- wsgi.py
+++ wsgi.py
@@ -1,38 +1,37 @@
 #!/usr/bin/env python
-import os
-
 import gevent
 from gevent import monkey
 
 
 monkey.patch_all()
 
-
-virtenv = os.environ['OPENSHIFT_PYTHON_DIR'] + '/virtenv/'
-virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
-try:
-  # See: http://stackoverflow.com/questions/23418735/using-python-3-3-in-openshifts-book-example?noredirect=1#comment35908657_23418735
-  #execfile(virtualenv, dict(__file__=virtualenv)) # for Python v2.7
-  #exec(compile(open(virtualenv, 'rb').read(), virtualenv, 'exec'), dict(__file__=virtualenv)) # for Python v3.3
-  # Multi-Line for Python v3.3:
-  exec_namespace = dict(__file__=virtualenv)
-  with open(virtualenv, 'rb') as exec_file:
-    file_contents = exec_file.read()
-  compiled_code = compile(file_contents, virtualenv, 'exec')
-  exec(compiled_code, exec_namespace)
-except IOError:
-  pass
+import os
+# ~virtenv = os.environ['OPENSHIFT_PYTHON_DIR'] + '/virtenv/'
+# ~virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
+# ~try:
+  # ~# See: http://stackoverflow.com/questions/23418735/using-python-3-3-in-openshifts-book-example?noredirect=1#comment35908657_23418735
+  # ~#execfile(virtualenv, dict(__file__=virtualenv)) # for Python v2.7
+  # ~#exec(compile(open(virtualenv, 'rb').read(), virtualenv, 'exec'), dict(__file__=virtualenv)) # for Python v3.3
+  # ~# Multi-Line for Python v3.3:
+  # ~exec_namespace = dict(__file__=virtualenv)
+  # ~with open(virtualenv, 'rb') as exec_file:
+    # ~file_contents = exec_file.read()
+  # ~compiled_code = compile(file_contents, virtualenv, 'exec')
+  # ~exec(compiled_code, exec_namespace)
+# ~except IOError:
+  # ~pass
 
 
 from gevent.wsgi import WSGIServer
-from bottle_app import application
+from app import application
 
 
 ip = os.environ.get('OPENSHIFT_PYTHON_IP', 'localhost')
 port = int(os.environ.get('OPENSHIFT_PYTHON_PORT', 8051))
+host_name = os.environ.get('OPENSHIFT_GEAR_DNS', '')
 
-# ~application.run(host=ip, port=port, server='gevent')
-application.run(server='gevent')
+application.run(host=ip, port=port, server='gevent')
+# ~application.run(server='gevent')
 
 # ~http_server = WSGIServer((ip, port), application)
 # ~http_server.serve_forever()
