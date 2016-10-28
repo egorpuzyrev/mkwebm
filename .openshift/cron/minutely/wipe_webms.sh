1115860d986bd1048f7b6dae8978739a70c7d7c1
egorpuzyrev <egorpuzyrev@gmail.com>
2016-10-28 23:17:05 +0300

    Removed garbage comments. vp9 codec changed to vp8

--- .openshift/cron/minutely/wipe_webms.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-if [ ! -f $OPENSHIFT_DATA_DIR/last_run ]; then
-  touch $OPENSHIFT_DATA_DIR/last_run
-fi
-if [[ $(find $OPENSHIFT_DATA_DIR/last_run -mmin +29) ]]; then #run every 30 mins
-  rm -f $OPENSHIFT_DATA_DIR/last_run
-  touch $OPENSHIFT_DATA_DIR/last_run
-  find $OPENSHIFT_DATA_DIR/mkwebm/webms/ -type f -mtime +29 -exec rm {} \;
-fi
