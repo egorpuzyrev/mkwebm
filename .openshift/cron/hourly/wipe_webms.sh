c564222837a265e2fe963a4cdecb1e6706bfbd0f
egorpuzyrev <egorpuzyrev@gmail.com>
2016-10-28 23:16:23 +0300

    Changed wipe conditions

--- .openshift/cron/hourly/wipe_webms.sh
+++ .openshift/cron/hourly/wipe_webms.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-if [ ! -f $OPENSHIFT_DATA_DIR/last_run ]; then
-  touch $OPENSHIFT_DATA_DIR/last_run
-fi
-if [[ $(find $OPENSHIFT_DATA_DIR/last_run -mmin +29) ]]; then #run every 30 mins
-  touch $OPENSHIFT_DATA_DIR/last_run
-  find $OPENSHIFT_DATA_DIR/mkwebm/webms/ -type f -mmin +29 -exec rm {} \;
+HOURS=24
+if [[ $(find $OPENSHIFT_DATA_DIR/last_run -mmin +$(expr ${HOURS} \* 60)) ]]; then #run every 30 mins
+  find $OPENSHIFT_DATA_DIR/mkwebm/webms/ -type f -mmin +$(expr ${HOURS} \* 60) -exec rm {} \;
 fi
